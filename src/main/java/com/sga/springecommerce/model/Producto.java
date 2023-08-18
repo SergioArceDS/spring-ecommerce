e109a70ebca0229b7848656db82c273994f64ed1
Sergio <sergioarce777@hotmail.com>
2023-08-18 16:52:55 -0500

    Creacion de la estructura del proyecto y las clases modelo

---
+++ src/main/java/com/sga/springecommerce/model/Producto.java
@@ -0,0 +1,18 @@
+package com.sga.springecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private double precio;
+    private int cantidad;
+}
---
+++ src/main/java/com/sga/springecommerce/model/Usuario.java
@@ -0,0 +1,21 @@
+package com.sga.springecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+}
