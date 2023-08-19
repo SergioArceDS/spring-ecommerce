dbcaa77cdac91bd90d384b3be986d40d5e7965d5
Sergio <sergioarce777@hotmail.com>
2023-08-19 00:28:59 -0500

    Se crea repositorio, servicios y controlador para los productos

---
+++ src/main/java/com/sga/springecommerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.sga.springecommerce.repository;
+
+import com.sga.springecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
---
+++ src/main/java/com/sga/springecommerce/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.sga.springecommerce.service;
+
+import com.sga.springecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+
+    public Producto save(Producto producto);
+    public Optional<Producto> get(Integer id);
+    public void update(Producto producto);
+    public void delete(Integer id);
+}
