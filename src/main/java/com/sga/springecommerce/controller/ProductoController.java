90c6accae3ee0d684cf28629dec4619309242002
Sergio <sergioarce777@hotmail.com>
2023-08-19 19:43:49 -0500

    Se añade plantilla para crear productos, y se añade funcionalidad para guardar productos

--- src/main/java/com/sga/springecommerce/controller/ProductoController.java
+++ src/main/java/com/sga/springecommerce/controller/ProductoController.java
@@ -1,15 +1,44 @@
 package com.sga.springecommerce.controller;
 
+import com.sga.springecommerce.model.Producto;
+import com.sga.springecommerce.model.Usuario;
+import com.sga.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    @Autowired
+    private ProductoService productoService;
+
+    private final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+
     @GetMapping("")
     public String show(){
+
         return "productos/show";
+    }
+
+    @GetMapping("crear")
+    public String create(){
+
+        return "productos/create";
+    }
+
+    @PostMapping("/guardar")
+    public String save(Producto producto){
+        logger.info("Este es el objeto producto {}", producto);
+        Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
+
+        producto.setUsuario(usuario);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 }
--- src/main/java/com/sga/springecommerce/model/Usuario.java
+++ src/main/java/com/sga/springecommerce/model/Usuario.java
@@ -1,28 +1,33 @@
 package com.sga.springecommerce.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Usuario {
+    @NonNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NonNull
     private String nombre;
+    @NonNull
     private String username;
+    @NonNull
     private String email;
+    @NonNull
     private String direccion;
+    @NonNull
     private String telefono;
+    @NonNull
     private String tipo;
+    @NonNull
     private String password;
 
     @OneToMany(mappedBy = "usuario")
