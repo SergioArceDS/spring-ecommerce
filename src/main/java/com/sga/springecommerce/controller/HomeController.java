5d3fc7c3d38bec392f52007d514e77524e7a3d20
Sergio <sergioarce777@hotmail.com>
2023-08-23 23:14:35 -0500

    Mostrar la vista de la descripcion del  producto al usuario

--- src/main/java/com/sga/springecommerce/controller/HomeController.java
+++ src/main/java/com/sga/springecommerce/controller/HomeController.java
@@ -2,10 +2,13 @@ package com.sga.springecommerce.controller;
 
 import com.sga.springecommerce.model.Producto;
 import com.sga.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductoService productoService;
 
@@ -22,5 +27,12 @@ public class HomeController {
         model.addAttribute("productos", productos);
 
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        logger.info("Id del producto: {}", id);
+
+        return "usuario/productohome";
     }
 }
