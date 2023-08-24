a2f0e9031813f183c5ca3ebbbd8c53135f316ca8
Sergio <sergioarce777@hotmail.com>
2023-08-24 17:44:52 -0500

    Añadir los productos al carrtido y mostrarlos en la vista

--- src/main/java/com/sga/springecommerce/controller/HomeController.java
+++ src/main/java/com/sga/springecommerce/controller/HomeController.java
@@ -7,6 +7,7 @@ import com.sga.springecommerce.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -48,13 +49,27 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model){
         DetalleOrden detalleOrden = new DetalleOrden();
-        Producto producto = new Producto();
+        Producto producto;
         double sumaTotal = 0;
 
         Optional<Producto> optionalProducto = productoService.get(id);
         logger.info("Producto añadido {}", optionalProducto.get().getNombre());
+
+        producto = optionalProducto.get();
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
+
+        detalles.add(detalleOrden);
+
+        sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
         return "usuario/carrito";
     }
 }
