5937869c8663dca43bba5630d808f1dd815e1af2
Sergio <sergioarce777@hotmail.com>
2023-08-27 16:57:00 -0500

    Mostrar la vista de compras para el usuario

--- src/main/java/com/sga/springecommerce/controller/UsuarioController.java
+++ src/main/java/com/sga/springecommerce/controller/UsuarioController.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +57,12 @@ public class UsuarioController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(Model model, HttpSession session){
+
+        model.addAttribute("session", session.getAttribute("idUsuario"));
+        return "usuario/compras";
     }
 }
