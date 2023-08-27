845fe061a8bc1c3f210826e308b571f2f04c6fd5
Sergio <sergioarce777@hotmail.com>
2023-08-26 19:50:53 -0500

    Obtener usuario de la base de datos para el login y agregarlo a la session

--- src/main/java/com/sga/springecommerce/controller/UsuarioController.java
+++ src/main/java/com/sga/springecommerce/controller/UsuarioController.java
@@ -2,15 +2,21 @@ package com.sga.springecommerce.controller;
 
 import com.sga.springecommerce.model.Usuario;
 import com.sga.springecommerce.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private IUsuarioService usuarioService;
@@ -24,6 +30,30 @@ public class UsuarioController {
     public String guardarUsuario(Usuario usuario){
         usuario.setTipo("USER");
         usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session){
+
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+
+        if(user.isPresent()){
+            session.setAttribute("idUsuario", user.get().getId());
+            if(user.get().getTipo().equals("ADMIN")){
+                return "redirect:/administrador";
+            }else{
+                return "redirect:/";
+            }
+        }else{
+            logger.info("El Usuario no existe");
+        }
+
         return "redirect:/";
     }
 }
