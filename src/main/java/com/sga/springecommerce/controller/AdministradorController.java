5fbdabc925206f2b665ce799f8b2b8fe4afeb2de
Sergio <sergioarce777@hotmail.com>
2023-08-29 14:26:47 -0500

    Funcionalidad para ver las ordenes desde el perfil administrador

--- src/main/java/com/sga/springecommerce/controller/AdministradorController.java
+++ src/main/java/com/sga/springecommerce/controller/AdministradorController.java
@@ -2,9 +2,11 @@ package com.sga.springecommerce.controller;
 
 import com.sga.springecommerce.model.Producto;
 import com.sga.springecommerce.model.Usuario;
+import com.sga.springecommerce.service.IOrdenService;
 import com.sga.springecommerce.service.IProductoService;
 import com.sga.springecommerce.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,9 @@ public class AdministradorController {
     @Autowired
     private IUsuarioService usuarioService;
 
+    @Autowired
+    private IOrdenService ordenService;
+
     @GetMapping("")
     public String home(Model model){
         List<Producto> productos = productoService.findAll();
@@ -36,5 +41,12 @@ public class AdministradorController {
         model.addAttribute("usuarios", usuarios);
 
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model){
+        model.addAttribute("ordenes", ordenService.findAll());
+
+        return "administrador/ordenes";
     }
 }
